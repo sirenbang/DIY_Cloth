848d6327e7cad09370856a50a43acccc8aa60fd4
Si RenBang <sirenbangwzzz@163.com>
2013-04-23 00:46:26 +0800

    new

---
+++ src/com/srb/diyCloth/util/HibernateUtil.java
@@ -0,0 +1,102 @@
+package com.srb.diyCloth.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static String CONFIG_FILE_LOCATION = "/hibernate.cfg.xml";
+	private static final ThreadLocal<Session> threadLocal = new ThreadLocal<Session>();
+	private static Configuration configuration = new Configuration();
+	private static org.hibernate.SessionFactory sessionFactory;
+	private static String configFile = CONFIG_FILE_LOCATION;
+
+	static {
+		try {
+			configuration.configure(configFile);
+			sessionFactory = configuration.buildSessionFactory();
+		} catch (Exception e) {
+			System.err.println("%%%% Error Creating SessionFactory %%%%");
+			e.printStackTrace();
+		}
+	}
+
+	private HibernateUtil() {
+	}
+
+	/**
+	 *	获得 Session
+	 * 
+	 * @return Session
+	 * @throws HibernateException
+	 */
+	public static Session getSession() throws HibernateException {
+		Session session = (Session) threadLocal.get();
+
+		if (session == null || !session.isOpen()) {
+			if (sessionFactory == null) {
+				rebuildSessionFactory();
+			}
+			session = (sessionFactory != null) ? sessionFactory.openSession()
+					: null;
+			threadLocal.set(session);
+		}
+
+		return session;
+	}
+
+	/**
+	 * 重构 hibernate session factory
+	 * 
+	 */
+	public static void rebuildSessionFactory() {
+		try {
+			configuration.configure(configFile);
+			sessionFactory = configuration.buildSessionFactory();
+		} catch (Exception e) {
+			System.err.println("%%%% Error Creating SessionFactory %%%%");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 关闭 Session
+	 * 
+	 * @throws HibernateException
+	 */
+	public static void closeSession() throws HibernateException {
+		Session session = (Session) threadLocal.get();
+		threadLocal.set(null);
+
+		if (session != null) {
+			session.close();
+		}
+	}
+
+	/**
+	 * 获得 Session Factory
+	 * 
+	 */
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * 设置配置文件的位置
+	 */
+	public static void setConfigFile(String configFile) {
+		HibernateUtil.configFile = configFile;
+		sessionFactory = null;
+	}
+
+	/**
+	 * 返回 hibernate 的配置
+	 * 
+	 */
+	public static Configuration getConfiguration() {
+		return configuration;
+	}
+
+}
